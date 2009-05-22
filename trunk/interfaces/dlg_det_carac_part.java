944ebfa483942ed89fb184c18136bfffa85c29f6
leandroroscitto <leandroroscitto@759caec4-44ba-11de-b25d-9f9778916a3c>
2009-05-22 21:58:54 +0000

    Las tres ultimas interfaces gráficas que faltaban.

---
+++ trunk/interfaces/dlg_det_carac_part.java
@@ -0,0 +1,164 @@
+package interfaces;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.GroupLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.LayoutStyle;
+import javax.swing.SpinnerListModel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class dlg_det_carac_part extends javax.swing.JDialog {
+	private JComboBox cb_carc_part;
+	private JFormattedTextField ftxf_arancel;
+	private JCheckBox cbx_e_cert;
+	private JLabel lb_porcent;
+	private JLabel lb_arancel;
+	private JLabel lb_carac_part;
+	private JSpinner sp_porcent;
+	private JCheckBox cbx_eva;
+
+	{
+		//Set Look & Feel
+		try {
+			javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	* Auto-generated main method to display this JDialog
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame();
+				dlg_det_carac_part inst = new dlg_det_carac_part(frame);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public dlg_det_carac_part(JFrame frame) {
+		super(frame);
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
+			getContentPane().setLayout(thisLayout);
+			this.setTitle("Detalles de Caracter de Participación");
+			{
+				ComboBoxModel cb_carc_partModel = 
+					new DefaultComboBoxModel(
+							new String[] { "Item One", "Item Two" });
+				cb_carc_part = new JComboBox();
+				cb_carc_part.setModel(cb_carc_partModel);
+			}
+			{
+				ftxf_arancel = new JFormattedTextField();
+				ftxf_arancel.setText("$ 0,00");
+			}
+			{
+				cbx_eva = new JCheckBox();
+				cbx_eva.setText("Es evaluado:");
+				cbx_eva.setHorizontalTextPosition(SwingConstants.LEFT);
+			}
+			{
+				cbx_e_cert = new JCheckBox();
+				cbx_e_cert.setText("Emite certificado");
+				cbx_e_cert.setHorizontalTextPosition(SwingConstants.LEFT);
+			}
+			{
+				SpinnerListModel sp_porcentModel = 
+					new SpinnerListModel(
+							new String[] { "0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"});
+				sp_porcent = new JSpinner();
+				sp_porcent.setModel(sp_porcentModel);
+			}
+			{
+				lb_carac_part = new JLabel();
+				lb_carac_part.setText("Caracter de Participación:");
+			}
+			{
+				lb_arancel = new JLabel();
+				lb_arancel.setText("Monto de arancel:");
+			}
+			{
+				lb_porcent = new JLabel();
+				lb_porcent.setText("Porcentaje mínimo de asistencia:");
+			}
+			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+				.addGap(8)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(cb_carc_part, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(lb_carac_part, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(ftxf_arancel, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(lb_arancel, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addComponent(cbx_eva, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(cbx_e_cert, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(sp_porcent, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(lb_porcent, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addGap(6));
+			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap(12, 12)
+				.addGroup(thisLayout.createParallelGroup()
+				    .addGroup(thisLayout.createSequentialGroup()
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addComponent(lb_carac_part, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(cbx_eva, GroupLayout.PREFERRED_SIZE, 161, GroupLayout.PREFERRED_SIZE)
+				                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED))
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(cbx_e_cert, GroupLayout.PREFERRED_SIZE, 161, GroupLayout.PREFERRED_SIZE)
+				                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED))
+				            .addComponent(lb_arancel, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE))
+				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addComponent(cb_carc_part, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(ftxf_arancel, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+				                .addGap(69))))
+				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				        .addComponent(lb_porcent, GroupLayout.PREFERRED_SIZE, 203, GroupLayout.PREFERRED_SIZE)
+				        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				        .addComponent(sp_porcent, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+				        .addGap(0, 25, Short.MAX_VALUE)))
+				.addContainerGap());
+			pack();
+			this.setSize(330, 169);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ trunk/interfaces/dlg_reg_resp.java
@@ -0,0 +1,136 @@
+package interfaces;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle;
+import javax.swing.SwingUtilities;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class dlg_reg_resp extends javax.swing.JDialog {
+	private JLabel lb_selec_resp;
+	private JTable tbl_personas;
+	private JScrollPane sp_tabla;
+	private JButton btn_cancelar;
+	private JButton btn_registrar_pers;
+	private JButton btn_agregar_resp;
+	private JPanel pnl_botones;
+
+	{
+		//Set Look & Feel
+		try {
+			javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	/**
+	* Auto-generated main method to display this JDialog
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame();
+				dlg_reg_resp inst = new dlg_reg_resp(frame);
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public dlg_reg_resp(JFrame frame) {
+		super(frame);
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			{
+				GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
+				getContentPane().setLayout(thisLayout);
+				this.setTitle("Registro de Responsables");
+				this.setPreferredSize(new java.awt.Dimension(565, 205));
+				this.setResizable(false);
+				this.setMinimumSize(new java.awt.Dimension(565, 205));
+				{
+					lb_selec_resp = new JLabel();
+					lb_selec_resp.setText("Seleccionar Responsable:");
+					lb_selec_resp.setSize(265, 27);
+				}
+				{
+					sp_tabla = new JScrollPane();
+					sp_tabla.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+					{
+						TableModel tbl_personasModel = 
+							new DefaultTableModel(new String[] { "Nombre", "Apellido", "E-mail" },8);
+						tbl_personas = new JTable();
+						sp_tabla.setViewportView(tbl_personas);
+						tbl_personas.setModel(tbl_personasModel);
+						tbl_personas.setPreferredSize(new java.awt.Dimension(389, 128));
+					}
+				}
+				{
+					pnl_botones = new JPanel();
+					FlowLayout pnl_botonesLayout = new FlowLayout();
+					pnl_botones.setLayout(pnl_botonesLayout);
+					{
+						btn_agregar_resp = new JButton();
+						pnl_botones.add(btn_agregar_resp);
+						btn_agregar_resp.setText("Agregar Responsable");
+					}
+					{
+						btn_registrar_pers = new JButton();
+						pnl_botones.add(btn_registrar_pers);
+						btn_registrar_pers.setText("Registrar Persona");
+					}
+					{
+						btn_cancelar = new JButton();
+						pnl_botones.add(btn_cancelar);
+						btn_cancelar.setText("Cancelar");
+					}
+				}
+					thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+						.addComponent(lb_selec_resp, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(sp_tabla, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
+						.addComponent(pnl_botones, 0, 41, Short.MAX_VALUE));
+					thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+						.addGap(8)
+						.addGroup(thisLayout.createParallelGroup()
+						    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+						        .addComponent(lb_selec_resp, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE)
+						        .addGap(0, 365, Short.MAX_VALUE))
+						    .addComponent(sp_tabla, GroupLayout.Alignment.LEADING, 0, 531, Short.MAX_VALUE)
+						    .addComponent(pnl_botones, GroupLayout.Alignment.LEADING, 0, 531, Short.MAX_VALUE))
+						.addContainerGap());
+			}
+			pack();
+			this.setSize(565, 205);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
